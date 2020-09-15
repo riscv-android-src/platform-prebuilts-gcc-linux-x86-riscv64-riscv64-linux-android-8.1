bb72dcf0dd1da9cf6f80548087500606eb4f4bef
Lifang Xia <xlf194833@alibaba-inc.com>
2020-09-15 16:18:52 +0800

    add riscv GCC 8.1
    update libatomic for fPIC
    
    Change-Id: I12193bac799b74f7bd8453bb115fca336d4644f3
    Signed-off-by: CHEN GUOYIN <chenguoyin.cgy@alibaba-inc.com>

---
+++ lib/gcc/riscv64-linux-android/8.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/lhome/xialf/riscv-gnu-toolchain/build-android/../riscv-gcc/configure --target=riscv64-linux-android --prefix=/lhome/xialf/riscv-gnu-toolchain/riscv64-linux-android --with-sysroot=/lhome/xialf/riscv-gnu-toolchain/riscv64-linux-android/sysroot --with-system-zlib --enable-shared --enable-tls --enable-languages=c,c++,fortran,go --disable-libmudflap --disable-libssp --disable-libquadmath --disable-nls --disable-bootstrap --src=../../riscv-gcc --with-pkgversion= --disable-multilib --with-abi=lp64d --with-arch=rv64imafdc 'CFLAGS_FOR_TARGET=-O2 -mno-relax -ffunction-sections  -mcmodel=medlow' 'CXXFLAGS_FOR_TARGET=-O2 -mno-relax -ffunction-sections  -mcmodel=medlow' CC=gcc CXX=g++";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64d" }, { "arch", "rv64imafdc" } };
